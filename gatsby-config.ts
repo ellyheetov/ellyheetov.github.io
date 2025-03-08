1dd74621b2bbdb085447bfe4a666e27192910f15
Hyewon Park <hyeone.park@navercorp.com>
2025-03-08 13:42:17 +0900

    chore: change blog description

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,9 +8,9 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
+    siteTitle: `ellyheetov`,
+    siteTitleAlt: `ellyheetov Blog - Gatsby Theme`,
+    siteHeadline: `ellyheetov - Gatsby Theme from @lekoarts`,
     siteUrl: `https://minimal-blog.lekoarts.de`,
     siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
     siteImage: `/banner.jpg`,
@@ -34,14 +34,6 @@ const config: GatsbyConfig = {
           },
         ],
         externalLinks: [
-          {
-            name: `Bluesky`,
-            url: `https://bsky.app/profile/lekoarts.de`,
-          },
-          {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
-          },
         ],
       },
     },
